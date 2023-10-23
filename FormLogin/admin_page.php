e93f7f1735fcf120bacd4ac645690928fe05c8ed
apiip0709 <afifalhaq777@gmail.com>
2023-10-24 07:43:07 +0800

    fix project form login

--- FormLogin/admin_page.php
+++ FormLogin/admin_page.php
@@ -1,3 +1,15 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['admin_name'])){
+    header('location:login_form.php');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,13 +18,13 @@
     <title>Login | Admin Page</title>
 
     <!-- Custom css file link -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <div class="container">
         <div class="content">
             <h3>hi, <span>admin</span></h3>
-            <h1>welcome <span></span></h1>
+            <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
             <p>this is an admin page</p>
             <a href="login_form.php" class="btn">login</a>
             <a href="register_form.php" class="btn">register</a>
--- FormLogin/user_page.php
+++ FormLogin/user_page.php
@@ -1,3 +1,15 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['user_name'])){
+    header('location:login_form.php');
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,17 +18,17 @@
     <title>Login | User Page</title>
 
     <!-- Custom css file link -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <div class="container">
         <div class="content">
             <h3>hi, <span>user</span></h3>
-            <h1>welcome <span></span></h1>
+            <h1>welcome <span><?php echo $_SESSION['user_name'] ?></span></h1>
             <p>this is an user page</p>
             <a href="login_form.php" class="btn">login</a>
             <a href="register_form.php" class="btn">register</a>
-            <a href="logiut.php" class="btn">logout</a>
+            <a href="logout.php" class="btn">logout</a>
         </div>
     </div>
 </body>
